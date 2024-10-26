4bb921fda054a77a82e15384b0d474fcca285bef
AlexeiEvenepoel <e_2019200797H@uncp.edu.pe>
2024-10-26 15:13:21 -0500

    layout done

--- src/app/shared/ui/layout.component.ts
+++ src/app/shared/ui/layout.component.ts
@@ -7,7 +7,17 @@ import { AuthStateService } from '../data-access/auth-state.service';
   standalone: true,
   imports: [RouterModule],
   // templateUrl: './layout.component.html',
-  template: ` <button (click)="logOut()">Logout</button>
+  template: `<header class="h-[80px] mb-8 w-full max-w-screen-lg mx-auto px-4">
+      <nav class="flex items-center justify-between h-full">
+        <a class="text-2xl font-bold" routerLink="/tasks">Ng Task</a>
+        <button
+          type="button"
+          class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+        
+          (click)="logOut()">Logout
+        </button>
+      </nav>
+    </header>
     <router-outlet />`,
 })
 export default class LayoutComponent {
---
+++ src/app/task/features/task-form/task-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './task-form.component.html',
+  styles: ``
+})
+export class TaskFormComponent {
+
+}
