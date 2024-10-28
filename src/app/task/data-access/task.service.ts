96000264f017a26a83ed808c0b503a392fc6d9d4
AlexeiEvenepoel <e_2019200797H@uncp.edu.pe>
2024-10-27 19:53:11 -0500

    create edit show tasks done

---
+++ src/app/task/data-access/task.service.ts
@@ -0,0 +1,63 @@
+import { inject, Injectable, signal } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+import {
+  Firestore,
+  collection,
+  addDoc,
+  collectionData,
+  doc,
+  getDoc,
+  updateDoc,
+  deleteDoc,
+} from '@angular/fire/firestore';
+import { catchError, Observable, tap, throwError } from 'rxjs';
+
+export interface Task {
+  id: string;
+  title: string;
+  completed: boolean;
+}
+
+export type TaskCreate = Omit<Task, 'id'>;
+
+const PATH = 'tasks';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TaskService {
+  private _firestore = inject(Firestore);
+
+  private _collection = collection(this._firestore, PATH);
+
+  loading = signal<boolean>(true);
+
+  getTasks = toSignal(
+    (
+      collectionData(this._collection, { idField: 'id' }) as Observable<Task[]>
+    ).pipe(
+      tap(() => {
+        this.loading.set(false);
+      }),
+      catchError((error) => {
+        this.loading.set(false);
+        return throwError(() => error);
+      })
+    ),
+    { initialValue: [] }
+  );
+
+  getTask(id: string) {
+    const docRef = doc(this._collection, id);
+    return getDoc(docRef);
+  }
+
+  create(task: TaskCreate) {
+    return addDoc(this._collection, task);
+  }
+
+  update(task: TaskCreate, id: string) {
+    const docRef = doc(this._collection, id);
+    return updateDoc(docRef, task);
+  }
+}
---
+++ src/app/task/ui/table/table.component.ts
@@ -0,0 +1,19 @@
+import { Component, effect, input } from '@angular/core';
+import { Task } from '../../data-access/task.service';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-table',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './table.component.html',
+})
+export class TableComponent {
+  tasks = input.required<Task[]>();
+  // ver info en consola
+  // constructor() {
+  //   effect(() => {
+  //     console.log(this.tasks());
+  //   })
+  // }
+}
